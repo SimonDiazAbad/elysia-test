35e57c34dd130598d4e9864f883aa4e3f8693d0d
Simon <simon.diaz.abad@gmail.com>
2024-08-07 16:46:16 -0400

    formatted dto

--- src/controllers/v1/users/users.dto.ts
+++ src/controllers/v1/users/users.dto.ts
@@ -1,14 +1,20 @@
 import { t } from 'elysia';
 
-export const GetUserByIdParamsSchema = t.Object({
+export const GetUserByIdParamsSchema = t.Object(
+  {
     id: t.String(),
-}, {
+  },
+  {
     description: 'Expects user ID',
-});
+  }
+);
 
-export const GetUserByIdResponseSchema = t.Object({
+export const GetUserByIdResponseSchema = t.Object(
+  {
     id: t.String(),
     name: t.String(),
-}, {
+  },
+  {
     description: 'Returns user info',
-})
+  }
+);
